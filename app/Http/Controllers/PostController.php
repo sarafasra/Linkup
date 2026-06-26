558f7e23c9d69c00c748f439d73293bd04dde43d
sarafasra <sarafasraoui75@gmail.com>
2026-06-26 16:56:20 +0100

    Add PostController with feed index method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 
 class PostController extends Controller
-{
+{ 
     public function index()
     {
         $posts = Post::with('user')->latest()->get();
