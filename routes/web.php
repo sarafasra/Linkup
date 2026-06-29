787ec782a9493a111086daa58d753416303f84c1
sarafasra <sarafasraoui75@gmail.com>
2026-06-29 17:20:36 +0100

    creer un nouveau branche

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::get('/feed', [PostController::class, 'index']);
+
+
+
+
+Route::get('/register',[AuthController::class,'showRegister'])->name('register');
+Route::post('/register',[AuthController::class,'register']);
+
+
+
+
+Route::get('/login',[AuthController::class,'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
