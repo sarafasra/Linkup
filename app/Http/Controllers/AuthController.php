5c262c197f06667c9560d8e37e53b9e80d8cc27d
sarafasra <sarafasraoui75@gmail.com>
2026-06-30 16:29:14 +0100

    creer un fichier register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;  
-
   
 class AuthController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@ Route::post('/register',[AuthController::class,'register']);
 
 
 
+
+Route::get('/login',[AuthController::class,'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
