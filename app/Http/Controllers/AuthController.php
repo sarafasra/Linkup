0961c37c92034a1e55c075926d38be928b324ff6
sarafasra <sarafasraoui75@gmail.com>
2026-06-30 17:28:48 +0100

    Add AuthController with register and login views

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;  
-  
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 class AuthController extends Controller
 {
-    public function showRegister(){
+    public function showRegister()
+    {
         return view('auth.register');
     }
-    public function showLogin(){
+
+    public function showLogin()
+    {
         return view('auth.login');
     }
-    
+
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+             'headline' => 'required|min:8|max:255',
+             'password' => 'required|min:8|confirmed',
+        ]);
+
+        $user =User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'headline' => $request->headline,
+            'password' => Hash::make($request->password),
+        ]);
+        Auth::login($user);
+        return redirect('/feed');
+    }
 }
