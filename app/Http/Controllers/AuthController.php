b0fb82f9da374ac1ea15b7785e7a75f153a48019
sarafasra <sarafasraoui75@gmail.com>
2026-06-30 15:36:15 +0100

    modifier dans le route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;  
+
   
 class AuthController extends Controller
 {
     public function showRegister(){
         return view('auth.register');
     }
-    public function showLigin(){
+    public function showLogin(){
         return view('auth.login');
     }
     
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,3 @@ Route::post('/register',[AuthController::class,'register']);
 
 
 
-
-Route::get('/login',[AuthController::class,'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
