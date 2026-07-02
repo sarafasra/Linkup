ac6be56903216dc7b206352714dc670ba3e68a84
sarafasra <sarafasraoui75@gmail.com>
2026-07-02 15:56:06 +0100

    ajouter les routes des methode edit et update dans le fichier web.php

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/feed', [PostController::class, 'index'])->name('feed.index');
     Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit' , [PostController::class, 'edit'])->name(('posts.edit'));
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 });
 
 
