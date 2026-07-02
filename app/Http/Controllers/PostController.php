541e16c1f5ed49083d05c84d58926b9210714bea
sarafasra <sarafasraoui75@gmail.com>
2026-07-02 12:42:30 +0100

    Implement post creation with form request validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 { 
@@ -19,7 +20,11 @@ class PostController extends Controller
     return view ('posts.create');
     }
 
-    public function store(){
-
+public function store(StorePostRequest $request){
+                   Post::create([
+                    'content' => $request->content,
+                    'user_id' => Auth::id(),
+                   ]);
+                   return redirect()->route('feed.index');
     }
 }    
--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -12,7 +12,7 @@ class StorePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,7 @@ class StorePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'content' => 'required|min:10',
         ];
     }
 }
