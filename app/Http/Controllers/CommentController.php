9ae71dcf1af643b861fdb9a25b1d04c831bd5fcb
sarafasra <sarafasraoui75@gmail.com>
2026-07-08 09:53:20 +0100

    ajouter un conytroller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,20 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCommentRequest;
 use App\Models\Comment;
 use App\Models\Post;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    public function store(Request $request, Post $post){ 
-    Comment::create([
-        'content' =>$request->content,
-         'user_id' =>Auth::id(),
-         'post_id' =>$post->id,
+    public function store(StoreCommentRequest $request, Post $post)
+    {
+        Comment::create([
+            'content' => $request->content,
+            'user_id' => Auth::id(),
+            'post_id' => $post->id,
+        ]);
 
-    ]);
-
-    return redirect()->back();
+        return back();
     }
-
 }
