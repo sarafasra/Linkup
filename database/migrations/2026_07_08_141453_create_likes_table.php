e1ebb27aed9e583b74cdce1f16ab599ba58295c3
sarafasra <sarafasraoui75@gmail.com>
2026-07-09 11:26:54 +0100

    ajouter le bouton Like et le compteur dans le feed

--- database/migrations/2026_07_08_141453_create_likes_table.php
+++ database/migrations/2026_07_08_141453_create_likes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-
+ 
             $table->foreignId('user_id')->constrained()->cascadeOnUpdate();
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
