e0873a3f2437ad196e27fb5659016fd4962beb6b
sarafasra <sarafasraoui75@gmail.com>
2026-07-09 11:27:14 +0100

    implémenter le système de like et unlike

--- database/migrations/2026_07_08_141453_create_likes_table.php
+++ database/migrations/2026_07_08_141453_create_likes_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('likes', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
  
             $table->foreignId('user_id')->constrained()->cascadeOnUpdate();
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
